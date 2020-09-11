6863c7099ef22ab7468f8f8edb610bb151db371f
1337x <18002848@vcconnect.co.za>
2020-09-11 22:12:32 +0000

    Initial commit

---
+++ app/src/main/java/com/example/burgerorders19010230/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.burgerorders18002848;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
